0af476ed18ceb7f568dc71b1ece11d2107c7882c
unknown <juanmena54>
2022-08-21 15:33:17 -0400

    entrega

---
+++ main.py
@@ -0,0 +1,21 @@
+import json
+
+list = []
+
+with open('farmers-protest-tweets-2021-03-5.json', 'r') as file:
+  data = json.load(file)
+  print(data)
+
+def function_1():
+  print()
+
+def function_2():
+  pass
+
+def function_3():
+  pass
+
+def function_4():
+  pass
+
+function_1()
